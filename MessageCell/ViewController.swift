af518c408150a287f31753adf188fe7366dad7fa
Ray <hopechenchen@gmail.com>
2021-07-04 10:50:39 +0800

    Initial Commit

---
+++ MessageCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessageCell
+//
+//  Created by Ray on 2021/7/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
